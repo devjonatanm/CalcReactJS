99cba7d70c8abe18ea9b921dd945c58cfcd2546a
Jonatan Marques <jonatanmarsv@gmail.com>
2023-08-10 18:09:47 -0300

    parte 1 estilização finalizada

--- src/assets/Calculator/index.jsx
+++ src/assets/Calculator/index.jsx
@@ -1,32 +1,46 @@
+import { useState } from "react";
 import "./style.css";
 
 export function Calculator() {
+  const [count, setCount] = useState(0)
+
+  /* const setValue = () => {
+    setCount(prevState => prevState + 1)
+  } */
+
+
   return (
     <div className="container">
-      <div className="line">0000000</div>
       <div className="line">
-        <div className="key">BACK</div>
-        <div className="key">CE</div>
-        <div className="key">C</div>
+        <div className="display">{count}</div>
       </div>
       <div className="line">
-        <div onClick={console.log(7+7)} className="key">7</div>
+        <div className="key">⬅️</div>
+        <div className="key">CE</div>
+        <div className="key">C</div>
+        <div className="key">+/-</div>
+      </div>
+      <div className="line">
+        <div onClick={event => setCount(event.target.textContent)} className="key">7</div>
         <div className="key">8</div>
         <div className="key">9</div>
+        <div className="key">/</div>
       </div>
       <div className="line">
         <div className="key">4</div>
         <div className="key">5</div>
         <div className="key">6</div>
+        <div className="key">*</div>
       </div>
       <div className="line">
         <div className="key">1</div>
         <div className="key">2</div>
         <div className="key">3</div>
+        <div className="key">-</div>
       </div>
       <div className="line">
-        <div className="key">0</div>
-        <div className="key">,</div>
+        <div className="key2">0</div>
+        <div className="key">-</div>
         <div className="key">+</div>
       </div>
     </div>
